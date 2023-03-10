b58c01a1110349c543f3863e7c6003c877d69f18
Andrew C <127004647+andrewc0de@users.noreply.github.com>
2023-03-10 06:53:46 -0500

    Update venyx-docs.lua

--- Dependencies/venyx-docs.lua
+++ Dependencies/venyx-docs.lua
@@ -1,36 +1,63 @@
 
 -- init
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MagikManz/Venyx-UI-Library/main/source.lua"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
+local venyx = library.new("Venyx", 5013109572)
 
-local ui = UI.new("Dino Smells")
+-- themes
+local themes = {
+	Background = Color3.fromRGB(24, 24, 24),
+	Glow = Color3.fromRGB(0, 0, 0),
+	Accent = Color3.fromRGB(10, 10, 10),
+	LightContrast = Color3.fromRGB(20, 20, 20),
+	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	TextColor = Color3.fromRGB(255, 255, 255)
+}
 
-local tab = ui:Tab("Wally sucks")
-ui:Tab("Fuck"):Section("Wally needs to get friends")
+-- first page
+local page = venyx:addPage("Test", 5012544693)
+local section1 = page:addSection("Section 1")
+local section2 = page:addSection("Section 2")
 
-local section = tab:Section("Nexure is a loser")
+section1:addToggle("Toggle", nil, function(value)
+	print("Toggled", value)
+end)
+section1:addButton("Button", function()
+	print("Clicked")
+end)
+section1:addTextbox("Notification", "Default", function(value, focusLost)
+	print("Input", value)
 
-section:Toggle("Juniortrackob", false, function(toggle)
-	print("yea", toggle)
+	if focusLost then
+		venyx:Notify("Title", value)
+	end
 end)
 
-tab:Toggle("You can also have toggles with no tab", false, function(toggle)
-	print("mhm", toggle)
+section2:addKeybind("Toggle Keybind", Enum.KeyCode.One, function()
+	print("Activated Keybind")
+	venyx:toggle()
+end, function()
+	print("Changed Keybind")
 end)
-
-section:Slider("Yoo kikitob stinks", 0, 900, 100, function(value)
-	print(value)
+section2:addColorPicker("ColorPicker", Color3.fromRGB(50, 50, 50))
+section2:addColorPicker("ColorPicker2")
+section2:addSlider("Slider", 0, -100, 100, function(value)
+	print("Dragged", value)
 end)
-
-tab:ColourPicker("da colour picker (3dsboy08 has huge feet)", Color3.new(1, 1, 1), function(colour)
-	print("ayy", colour)
+section2:addDropdown("Dropdown", {"Hello", "World", "Hello World", "Word", 1, 2, 3})
+section2:addDropdown("Dropdown", {"Hello", "World", "Hello World", "Word", 1, 2, 3}, function(text)
+	print("Selected", text)
 end)
+section2:addButton("Button")
 
+-- second page
+local theme = venyx:addPage("Theme", 5012544693)
+local colors = theme:addSection("Colors")
 
-local section1 = tab:Section("Another section!")
-section1:Dropdown("Target", {"Head", "Torso", "Right Arm", "Left Arm", "Right Leg", "Left Leg"}, "Head", function(selected)
-	print(selected, "uhuh")
-end)
+for theme, color in pairs(themes) do -- all in one theme changer, i know, im cool
+	colors:addColorPicker(theme, color, function(color3)
+		venyx:setTheme(theme, color3)
+	end)
+end
 
-section:Key("Key", Enum.UserInputType.MouseButton2, function(key)
-	warn("sub")
-end)
+-- load
+venyx:SelectPage(venyx.pages[1], true)
